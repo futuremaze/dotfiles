eb46a96d9a0c9d55282249fb1883de9d6db34486
onodak <future.maze@gmail.com>
2023-12-20 22:56:03 +0900

    :feature: 各種プラグインの追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,3 +18,5 @@ vim.opt.rtp:prepend(lazypath)
 -- Example using a list of specs with the default options
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 require("lazy").setup("plugins")
+
+require("themes")
